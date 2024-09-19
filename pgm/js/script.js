b70753e8d4966b5a65d5b8335f29c2f9bc3e10d0
Masashi <masat4146@gmail.com>
2024-09-19 22:07:40 +0900

    responsive progress

---
+++ pgm/js/script.js
@@ -0,0 +1,113 @@
+/*
+** FileName: script.js
+** Description: js for Kenny's English website
+** Date: 2024/9
+**
+*/
+document.addEventListener("DOMContentLoaded", function(event) { 
+  addEvent("int_des", typeText);
+});
+
+ function type(param) {
+    var setParam = param + "";
+    if (setParam.length < 2) {
+      return "0" + setParam;
+    } else {
+      return setParam;
+    }
+  }
+
+  function measure(cond) {
+    if (cond < 10) {
+      cond = "0" + cond;
+    }
+    return cond;
+  }
+  
+  // start timer
+  setInterval(timer, 1000);
+  
+  function timer() {
+    var cur = new Date();
+    var hh = cur.getHours();
+    var mm = cur.getMinutes();
+    var ss = cur.getSeconds();
+    mm = measure(mm);
+    ss = measure(ss);
+    document.getElementById('time').innerHTML = hh + ":" + mm + ":" + ss;
+    set = setTimeout(function() {
+      timer()
+    }, 500);
+  }
+
+  var typeText = [
+    "Kenny's English", 
+    "Private Lesson",
+    "Speaking",
+    "Listening",
+    "Reading",
+    "あなたのレベルに合わせたレッスン",
+    "教科書には載っていない方法で",
+    "英語を話せるようになりましょう"
+  ]
+
+var cond = 0,
+    stat = 0,
+    del = false,
+    br = false,
+    typeTime = 80,
+    typeInt = 1000,
+    typeMar = 1000,
+    typeDel = 20;
+
+function addEvent(hi, bell) {
+  var holdID = $("#" + hi),
+      statText = bell[stat],
+      mainText = holdID.children("h1"),
+      subText = holdID.children("p");
+
+  if (!del) {
+
+    if (cond < statText.length) {
+      if (statText.charAt(cond) == "|") {
+        br = true;
+        mainText.removeClass("bee");
+        subText.addClass("bee");
+        i++;
+        setTimeout(function(){ addEvent(hi, bell); }, typeMar);
+      } else {
+        if (!br) {
+          mainText.text(mainText.text() + statText.charAt(cond));
+        } else {
+          subText.text(subText.text() + statText.charAt(cond));
+        }
+        cond++;
+        setTimeout(function(){ addEvent(hi, bell); }, typeTime);
+      }
+
+    } else if (cond == statText.length) {
+      del = true;
+      setTimeout(function(){ addEvent(hi, bell); }, typeInt);
+    }
+
+  } else {
+
+    if (mainText.text().length > 0 || subText.text().length > 0) {
+      if (subText.text().length > 0) {
+        subText.text(subText.text().substring(0, subText.text().length - 1));
+      } else if (mainText.text().length > 0) {
+        subText.removeClass("bee");
+        mainText.addClass("bee");
+        mainText.text(mainText.text().substring(0, mainText.text().length - 1));
+      }
+      setTimeout(function(){ addEvent(hi, bell); }, typeDel);
+
+    } else { 
+      del = false;
+      cond = 0;
+      br = false;
+      stat = (stat + 1) % bell.length;
+      setTimeout(function(){ addEvent(hi, bell); }, 50);
+    }
+  }
+}
